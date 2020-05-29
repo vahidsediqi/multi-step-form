4782e9591e5344c0c043c6a3b6c6cf50eb8b7512
vahidsediqi <vsediqi@live.com>
2020-05-29 14:38:08 +0300

    form added

---
+++ main.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      step: 1,
+      totalsteps: 3,
+      errors: [],
+      members: [],
+      member: { 
+          name : '',
+          lastname: '',
+          email: ''
+      },
+      amount: 45.00
+    },
+    methods: {
+      add: function(){
+            this.members.push(this.member);
+            this.member = {};
+            this.step ++         
+      },
+      gottostep1: function(){
+          alert('You are goint to add a new member')
+          this.step = 1
+      },
+    
+      
+    }
+  });
+
+
